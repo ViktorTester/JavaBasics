4ee3c20320b9813db0d5821be3f92c045566f607
ViktorTester <viktor345@inbox.lv>
2024-06-30 17:46:18 +0300

    initial commit

---
+++ src/day1/Welcome.java
@@ -0,0 +1,17 @@
+// Пакет
+package day1;
+
+// Класс
+public class Welcome {
+
+    // Основной метод
+    public static void main(String[] args) {
+
+        // Вывод сообщения в терминал
+        System.out.println("Welcome to java");
+
+        // Вывод сообщения в терминал
+        System.out.println(10 + 20);
+    }
+
+}
