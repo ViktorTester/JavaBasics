ef182a7d1bce98687f05fe6648a088dd3b3ffb04
ViktorTester <viktor345@inbox.lv>
2024-07-20 20:19:27 +0300

    StringMethods file modified

--- src/day8/StringMethods.java
+++ src/day8/StringMethods.java
@@ -40,6 +40,42 @@ public class StringMethods {
         System.out.println("string length without spaces: " + s4.trim().length());  // 18
 
 
+// charAt() - возвращает конкретный символ строки по его индексу
+        String s5 = "Welcome";
 
+        System.out.println("The s5 string index 3 element is: " + s5.charAt(3)); // с
+        System.out.println("The s5 string last index element is: " + s5.charAt(6)); // e
+
+
+// contains() - проверка является ли одна строка подстрокой другой строки
+        String s6 = "Welcome World";
+        String s7 = "World";
+
+
+        // Метод всегда возвращает булево значение.
+        // Регистр символов учитывается
+
+        // Содержится ли в строке s6 подстрока s7
+        System.out.println(s6.contains(s7)); // true
+        System.out.println(s6.contains("world")); // false
+        System.out.println(s6.contains("wor")); // false
+        System.out.println(s6.contains("Wor")); // true
+        System.out.println(s6.contains("Welme")); // false
+
+
+// equals() - сравнение двух строк
+// equalsIgnoreCase() - сравнение двух строк (игнорируя регистр)
+
+        String s8 = "welcome";
+        String s9 = "welcome";
+        String s10 = "WElcome";
+
+        // Метод всегда возвращает булево значение
+
+        // Зачастую метод equals() идентичен "==", но не всегда
+        System.out.println(s8 == s9); // true
+        System.out.println(s8.equals(s9)); // true
+        System.out.println(s8.equals(s10)); // false
+        System.out.println(s8.equalsIgnoreCase(s10)); // true
     }
 }
