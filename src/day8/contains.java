b829e46c3ce6ac3cf84ab2197d8a893b4574f281
ViktorTester <viktor345@inbox.lv>
2024-07-21 17:52:33 +0300

    day8 directory refactored

---
+++ src/day8/contains.java
@@ -0,0 +1,23 @@
+package day8;
+
+public class contains {
+    public static void main(String[] args) {
+
+// contains()
+// проверка является ли одна строка подстрокой другой строки
+
+        String s6 = "Welcome World";
+        String s7 = "World";
+
+
+        // Метод всегда возвращает булево значение.
+        // Регистр символов учитывается
+
+        // Содержится ли в строке s6 подстрока s7
+        System.out.println(s6.contains(s7)); // true
+        System.out.println(s6.contains("world")); // false
+        System.out.println(s6.contains("wor")); // false
+        System.out.println(s6.contains("Wor")); // true
+        System.out.println(s6.contains("Welme")); // false
+    }
+}
---
+++ src/day8/length.java
@@ -0,0 +1,12 @@
+package day8;
+
+public class length {
+    public static void main(String[] args) {
+
+// length()
+// определение длины строки
+
+        String s = "Hello World";
+        System.out.println("s length is " + s.length()); // 11
+    }
+}
