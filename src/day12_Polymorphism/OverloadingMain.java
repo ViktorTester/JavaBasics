edf64014c685566f6249218b6bdba24189b487c3
ViktorTester <viktor345@inbox.lv>
2024-07-31 22:16:16 +0300

    day12 directory added

---
+++ src/day12_Polymorphism/OverloadingMain.java
@@ -0,0 +1,15 @@
+package day12_Polymorphism;
+
+public class OverloadingMain {
+    public static void main(String[] args) {
+
+        Overloading ov = new Overloading();
+
+        ov.sum(); // 1 метод
+        ov.sum(1, 2); // 2 метод
+        ov.sum(1, 15.16); // 3 метод
+        ov.sum(99.47, 56); // 4 метод
+        ov.sum(4, 6, 8); // 5 метод
+
+    }
+}
